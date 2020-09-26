95343c4b497cef267f2ea72f9aef927e5e68a4e1
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-09-27 00:12:14 +0200

    Adding properties to node

--- pathfinder/src/Utility/grid.ts
+++ pathfinder/src/Utility/grid.ts
@@ -1,22 +1,29 @@
 import { uuidv4 } from "./constants";
+import { IGrid, INodeProperties } from "./interface";
 
-interface IGrid {
-  generateBoard: (rows: number, columns: number) => Array<any>;
-  grid: Array<JSX.Element>;
-}
 export default class Grid implements IGrid {
   constructor(row: number, column: number) {
     this.grid = this.generateBoard(row, column);
   }
 
-  public grid: Array<any>;
+  public grid: Array<Array<INodeProperties>>;
+
+  addPropertiesToNode = (column: number, row: number): INodeProperties => {
+    let weight = Math.floor(Math.random() * 9) + 1;
+    return {
+      column,
+      row,
+      weight,
+      key: uuidv4(),
+    };
+  };
 
   generateBoard = (rows: number, columns: number): Array<any> => {
     let result: any = [];
-    for (let i = 0; i < columns; i++) {
+    for (let c = 0; c < columns; c++) {
       result.push([]);
-      for (let j = 0; j < rows; j++) {
-        result[i].push([uuidv4()]);
+      for (let r = 0; r < rows; r++) {
+        result[c].push(this.addPropertiesToNode(c + 1, r + 1));
       }
     }
     return result;
