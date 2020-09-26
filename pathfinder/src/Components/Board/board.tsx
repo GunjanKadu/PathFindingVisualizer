921fef47da43f4d75d6b6e6aec32e9c908521d4a
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-09-26 15:34:12 +0200

    Creating a board with unique Node ids and UUIDv4

--- pathfinder/src/Components/Board/board.tsx
+++ pathfinder/src/Components/Board/board.tsx
@@ -1,17 +1,49 @@
+import { spawn } from "child_process";
 import React from "react";
 import Node from "../Node/node";
 import "./board.css";
 
 function board() {
-  let rows = 10;
+  let rows = 50;
   let columns = 10;
 
-  const generateBoard = () => {};
+  const uuidv4 = () => {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (
+      c
+    ) {
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  };
+  const generateBoard = () => {
+    let result: any = [];
+    for (let i = 0; i < rows; i++) {
+      result.push([]);
+      for (let j = 0; j < columns; j++) {
+        let key = uuidv4();
+        result[i].push(
+          <span key={key} onClick={() => console.log(key)}>
+            <Node />
+          </span>
+        );
+      }
+    }
+
+    console.log(result);
+    return result;
+  };
 
   return (
     <div className="board">
       <h2>Board</h2>
-      <Node />
+      <div className="board___nodeContainer">
+        {generateBoard().map((item: Array<any>, i: number) => (
+          <div className="rows" key={i}>
+            {item.map((node, j) => node)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
