122f11650a166036ebd1b8eaac3ed7fd4c6d4c46
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-10-05 21:53:05 +0200

    Recursive division for walls

--- src/Utility/RandomWalls/Mazes.ts
+++ src/Utility/RandomWalls/Mazes.ts
@@ -18,3 +18,51 @@ export function randomWalls(grid: IGrid) {
 
 /* The below methods are used to generate a maze with
 the recursive dvision method.*/
+export function recursiveDivision(grid: IGrid) {
+  addInnerWalls(grid, true, 1, 47, 1, 17);
+  addOuterWalls(grid, 49, 19);
+}
+
+function addOuterWalls(grid: IGrid, width: number, height: number) {
+  for (var i = 0; i < height; i++) {
+    if (i === 0 || i === height - 1) {
+      for (var j = 0; j < width; j++) {
+        if (!grid.grid[i][j].isWall) grid.toggleWall(i, j);
+      }
+    } else {
+      if (!grid.grid[i][0].isWall) grid.toggleWall(i, 0);
+      if (!grid.grid[i][width - 1].isWall) grid.toggleWall(i, width - 1);
+    }
+  }
+}
+
+function addInnerWalls(
+  grid: IGrid,
+  h: boolean,
+  minX: number,
+  maxX: number,
+  minY: number,
+  maxY: number
+) {
+  if (h) {
+    if (maxX - minX < 2) {
+      return;
+    }
+
+    var y = Math.floor(randomNumber(minY, maxY) / 2) * 2;
+    addHWall(grid, minX, maxX, y);
+
+    addInnerWalls(grid, !h, minX, maxX, minY, y - 1);
+    addInnerWalls(grid, !h, minX, maxX, y + 1, maxY);
+  } else {
+    if (maxY - minY < 2) {
+      return;
+    }
+
+    var x = Math.floor(randomNumber(minX, maxX) / 2) * 2;
+    addVWall(grid, minY, maxY, x);
+
+    addInnerWalls(grid, !h, minX, x - 1, minY, maxY);
+    addInnerWalls(grid, !h, x + 1, maxX, minY, maxY);
+  }
+}
